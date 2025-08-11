3dda71603dbcc1c252fa6199be0baa1397f794b3
lineker <lineker.aguiar98@gmail.com>
2025-08-11 10:23:22 -0300

    Add first version of report static metrics agent

--- src/agents/srag_report_agent.py
+++ src/agents/srag_report_agent.py
@@ -5,10 +5,10 @@ from typing import Any, Dict, Optional
 
 from langgraph.graph import StateGraph, END, START
 
-from .schemas import MetricSeries
+from .schemas import MetricSeries, ReportAgentState, MetricsBundle
 from ..utils.report_queries import (
     SQL_CASE_GROWTH,
-    SQL_ICU,
+    SQL_UTI,
     SQL_MORTALITY,
     SQL_VACCINATION
 )
@@ -32,3 +32,120 @@ class SRAGMetricsReport:
             return [dict(zip(cols, r)) for r in rows] # Estruturar em uma lista de dicionário col: value
         finally:
             conn.close()
+        
+    def node_case_growth(self, state: ReportAgentState) -> ReportAgentState:
+        """_summary_
+
+        Args:
+            state (ReportAgentState): _description_
+
+        Returns:
+            ReportAgentState: _description_
+        """
+        rows = self._query_rows(SQL_CASE_GROWTH)
+        series = MetricSeries(name='case_growth', rows=rows, sql_used=SQL_CASE_GROWTH)
+        res = state["results"]
+        res['case_growth'] = series
+        return {"results": res}
+    
+    def node_mortality(self, state: ReportAgentState) -> ReportAgentState:
+        """_summary_
+
+        Args:
+            state (ReportAgentState): _description_
+
+        Returns:
+            ReportAgentState: _description_
+        """
+        rows = self._query_rows(SQL_MORTALITY)
+        series = MetricSeries(name='mortality_rate', rows=rows, sql_used=SQL_MORTALITY)
+        res = state["results"]
+        res['mortality_rate'] = series
+        return {"results": res}
+        
+    def node_uti(self, state: ReportAgentState) -> ReportAgentState:
+        """_summary_
+
+        Args:
+            state (ReportAgentState): _description_
+
+        Returns:
+            ReportAgentState: _description_
+        """
+        rows = self._query_rows(SQL_UTI)
+        series = MetricSeries(name='uti_utilization_rate', rows=rows, sql_used=SQL_UTI)
+        res = state["results"]
+        res['uti_utilization_rate'] = series
+        return {"results": res}
+    
+        
+    def node_vaccination(self, state: ReportAgentState) -> ReportAgentState:
+        """_summary_
+
+        Args:
+            state (ReportAgentState): _description_
+
+        Returns:
+            ReportAgentState: _description_
+        """
+        rows = self._query_rows(SQL_VACCINATION)
+        series = MetricSeries(name='vaccination_rate', rows=rows, sql_used=SQL_VACCINATION)
+        res = state['results']
+        res['vaccination_rate'] = series
+        return {"results": res}
+    
+    def node_aggregate_final(self, state: ReportAgentState):
+        """_summary_
+
+        Args:
+            state (ReportAgentState): _description_
+
+        Returns:
+            ReportAgentState: _description_
+        """
+        results = state['results']
+        bundle = MetricsBundle(
+            case_growth=results['case_growth'],
+            mortality_rate=results['mortality_rate'],
+            uti_utilization_rate=results["uti_utilization_rate"],
+            vaccination_rate=results["vaccination_rate"]
+            
+        )
+        return {"bundle": bundle}
+    
+    def build_graph(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        graph = StateGraph(ReportAgentState)
+        graph.add_node("metrics_case_growth", self.node_case_growth)
+        graph.add_node("metrics_mortality", self.node_mortality)
+        graph.add_node("metrics_icu", self.node_uti)
+        graph.add_node("metrics_vaccination", self.node_vaccination)
+        graph.add_node("aggregate_final", self.node_aggregate_final)
+
+        graph.add_edge(START, "metrics_case_growth")
+        graph.add_edge("metrics_case_growth", "metrics_mortality")
+        graph.add_edge("metrics_mortality", "metrics_icu")
+        graph.add_edge("metrics_icu", "metrics_vaccination")
+        graph.add_edge("metrics_vaccination", "aggregate_final")
+        graph.add_edge("aggregate_final", END)
+
+        return graph.compile()
+    
+    def run(self, start_date: Optional[date] = None, end_date: Optional[date] = None) -> MetricsBundle:
+        """_summary_
+
+        Args:
+            start_date (Optional[date], optional): _description_. Defaults to None.
+            end_date (Optional[date], optional): _description_. Defaults to None.
+
+        Returns:
+            MetricsBundle: _description_
+        """
+        app = self.build_graph()
+        init = {"start_date": start_date, "end_date": end_date, "results": {}}
+        output = app.invoke(init)
+        return output["bundle"]
