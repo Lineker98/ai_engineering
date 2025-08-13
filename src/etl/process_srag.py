58f10db93a3cfd017a72ad18293a7f9df151c930
lineker <lineker.aguiar98@gmail.com>
2025-08-13 17:50:20 -0300

    Add docstrings and format

--- src/etl/process_srag.py
+++ src/etl/process_srag.py
@@ -2,100 +2,110 @@ import pandas as pd
 from typing import List, Dict
 from pathlib import Path
 
-def convert_to_datetime(df: pd.DataFrame, columns_formats: Dict, errors='coerce') -> pd.DataFrame:
+
+def convert_to_datetime(
+    df: pd.DataFrame, columns_formats: Dict, errors="coerce"
+) -> pd.DataFrame:
     """
-    Convert especific column into datetime
+    Converte colunas específicas de um DataFrame para o formato datetime.
 
     Args:
-        df (pd.DataFrame): _description_
-        columns_formats (Dict): _description_
-        errors (str, optional): _description_. Defaults to 'coerce'.
+        df (pd.DataFrame): DataFrame contendo as colunas a serem convertidas.
+        columns_formats (Dict): Dicionário no formato {coluna: formato}, onde o formato segue o padrão strftime
+            ou None para inferência automática.
+        errors (str, optional): Estratégia de tratamento de erros ('ignore', 'raise', 'coerce').
+            Defaults to 'coerce'.
 
     Returns:
-        _type_: _description_
+        pd.DataFrame: O DataFrame original com as colunas convertidas para datetime.
     """
     for col, format in columns_formats.items():
         df[col] = pd.to_datetime(df[col], format=format, errors=errors)
-    print('Conversão de dados realizada!')
+    print("Conversão de dados realizada!")
     return df
 
-def load_df(df_path: str, sep=';', encoding='latin1', low_memory=False) -> pd.DataFrame:
+
+def load_df(df_path: str, sep=";", encoding="latin1", low_memory=False) -> pd.DataFrame:
     """
-    Loads a CSV file into a pandas DataFrame.
+    Carrega um arquivo CSV em um DataFrame do pandas.
 
     Args:
-        df_path (str): Path to the CSV file.
-        sep (str, optional): Delimiter to use. Defaults to ';'.
-        encoding (str, optional): File encoding. Defaults to 'latin1'.
-        low_memory (bool, optional): Internally process the file in chunks to lower memory usage. 
-                                     Defaults to False.
+        df_path (str): Caminho para o arquivo CSV.
+        sep (str, optional): Delimitador das colunas. Defaults to ';'.
+        encoding (str, optional): Codificação do arquivo. Defaults to 'latin1'.
+        low_memory (bool, optional): Reduz o uso de memória ao ler o arquivo em pedaços. Defaults to False.
 
     Returns:
-        pd.DataFrame: DataFrame containing the contents of the loaded CSV file.
+        pd.DataFrame: DataFrame contendo os dados carregados.
     """
     df = pd.read_csv(df_path, sep=sep, encoding=encoding, low_memory=low_memory)
-    print('Carregamento de dados realizado!')
+    print("Carregamento de dados realizado!")
     return df
+
 
 def store_df(df: pd.DataFrame, path: Path) -> None:
     """
-    Stores a pandas DataFrame as a CSV file at the specified path.
+    Salva um DataFrame em um arquivo CSV no caminho especificado.
 
     Args:
-        df (pd.DataFrame): The DataFrame to be saved.
-        path (Path): Full path (including filename) where the CSV file will be saved.
+        df (pd.DataFrame): DataFrame a ser salvo.
+        path (Path): Caminho completo (incluindo o nome do arquivo) para salvar o CSV.
 
-    Notes:
-        - Creates any missing parent directories automatically.
-        - The resulting CSV will not include the index column.
+    Returns:
+        None
     """
     # Garante que a pasta exista
     path.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(path, index=False)
 
+
 def select_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
     """
-    Returns a new DataFrame containing only the specified columns.
+    Seleciona colunas específicas de um DataFrame.
 
     Args:
-        df (pd.DataFrame): The original DataFrame.
-        columns (List[str]): A list of column names to select from the DataFrame.
+        df (pd.DataFrame): DataFrame original.
+        columns (List[str]): Lista com os nomes das colunas a serem selecionadas.
 
     Returns:
-        pd.DataFrame: A DataFrame with only the selected columns.
+        pd.DataFrame: Novo DataFrame contendo apenas as colunas selecionadas.
     """
     return df[columns]
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     COLUMNS_FORMATS = {
-        'DT_SIN_PRI': None,
-        'DT_NOTIFIC': None,
-        'DT_EVOLUCA': None,
-        'DT_ENTUTI': None,
-        'DT_SAIDUTI': None,
-        'DOSE_1_COV': "%d/%m/%Y",
-        'DOSE_2_COV': "%d/%m/%Y",
-        'DOSE_REF': "%d/%m/%Y",
-        'DOSE_2REF': "%d/%m/%Y"
+        "DT_SIN_PRI": None,
+        "DT_NOTIFIC": None,
+        "DT_EVOLUCA": None,
+        "DT_ENTUTI": None,
+        "DT_SAIDUTI": None,
+        "DOSE_1_COV": "%d/%m/%Y",
+        "DOSE_2_COV": "%d/%m/%Y",
+        "DOSE_REF": "%d/%m/%Y",
+        "DOSE_2REF": "%d/%m/%Y",
     }
-    COLUMNS_SELECTED = list(COLUMNS_FORMATS.keys()) + ['EVOLUCAO', 'HOSPITAL', 'UTI', 'VACINA_COV', 'CLASSI_FIN']
-    
-    # Diretórios
+    COLUMNS_SELECTED = list(COLUMNS_FORMATS.keys()) + [
+        "EVOLUCAO",
+        "HOSPITAL",
+        "UTI",
+        "VACINA_COV",
+        "CLASSI_FIN",
+    ]
+
     data_dir = Path("data")
     df_raw_path = data_dir / "raw" / "INFLUD25-04-08-2025.csv"
     output_path = data_dir / "staging" / "int_srag.csv"
-    
-    
+
     # Load the raw SRAG data
     df = load_df(df_raw_path)
-    
+
     # Convert the datetime columns
     df = convert_to_datetime(df=df, columns_formats=COLUMNS_FORMATS)
-    
+
     # Select the columns of interesst
     df = select_columns(df=df, columns=COLUMNS_SELECTED)
-    
+
     # Write the cleaned SRAG data
     store_df(df=df, path=output_path)
-    print('Processamento Concluído com Sucesso!')
+    print("Processamento Concluído com Sucesso!")
