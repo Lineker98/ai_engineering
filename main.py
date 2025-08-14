70b58dfcfc60e14da1f8eee98a5a5d386d990e1b
lineker <lineker.aguiar98@gmail.com>
2025-08-14 16:25:49 -0300

    Remove unused plot functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.agents.srag_sql_agent import SRAGSQLAgentApp
 from src.agents.srag_report_agent import SRAGMetricsReport
 from src.agents.srag_summary_agent import SummaryAgent
-from src.utils.plots import plot_daily_cases_last_30_days, plot_monthly_cases_last_12_months
 from src.etl.news_ingest import ingest_srag_news
 from dotenv import load_dotenv
 import os
@@ -23,17 +22,6 @@ plot_dir_metrics = 'src/report/imgs'
 
 agent_metrics_report = SRAGMetricsReport(sqlite_path=df_path)
 bundle = agent_metrics_report.run(save_path=metrics_output, plot_dir=plot_dir_metrics)
-
-# plot_daily_cases_last_30_days(
-#     df_path, "src/report/imgs/casos_diarios_30d_covid",
-#     where_clause="CLASSI_FIN = 5", 
-#     rolling_days=7, json_path='src/report/metrics/chart_last_30_daily_cases.json'
-# )
-
-# plot_monthly_cases_last_12_months(
-#     df_path, "src/report/imgs/casos_mensais_12m_covid",
-#     where_clause="CLASSI_FIN = 5", json_path='src/report/metrics/chart_last_12_montly_cases.json'
-# )
 
 
 # query = '("Síndrome Respiratória Aguda Grave" OR SRAG) (Brasil OR estados) (Covid OR Influenza OR VSR)'
