2aa0ef77dd2ca057184ecb6e881ea82133232c96
lineker <lineker.aguiar98@gmail.com>
2025-08-18 11:09:57 -0300

    remove unused paths

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from src.agents.orchestrator_agent import OrchestratorAgent
 from dotenv import load_dotenv
-import os
 import logging
 from src.utils.logs_config import setup_logging
 
 setup_logging()
 logger = logging.getLogger(__name__)
 load_dotenv()
-
-openai_api_key = os.getenv('OPENAI_API_KEY')
-df_path = 'data/marts/srag.sqlite'
-srag_news = "data/marts/srag_news.json"
-output_dir = "src/report"
 
 if __name__ == '__main__':
     logger.info("--- START SRAG REPORT ---")
